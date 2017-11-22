4ef702e5ab47fa9a690d5ca8ab7835b037fea1db
GeologU <geologu@yandex.ru>
2017-11-22 07:04:19 +0300

    add test for TextParams with empty indent and newline

--- with_html_ut.py
+++ with_html_ut.py
@@ -260,7 +260,7 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(n.node_raw.mode, 'line')
         self.assertEqual(n.node_raw.raw, '<!-- just a comment -->')
 
-    def test_page(self):
+    def make_page(self):
         page = with_html.HTMLNode()
         page.tagl('!DOCTYPE', xxhtml=None)
         page.rawl('')
@@ -287,6 +287,10 @@ class TestHTMLNode(unittest.TestCase):
                         else:
                             for item in [''] + [str(a) for a in range(1, 10)]:
                                 l.tagc('td', item)
+        return page
+
+    def test_debug_page(self):
+        page = self.make_page()
 
         self.maxDiff = None
         self.assertEqual(
@@ -319,6 +323,15 @@ class TestHTMLNode(unittest.TestCase):
     </body>
 </html>\n''',
             page.text(with_html.TextParams(level=0, offset='    ', newline='\n'))
+        )
+
+    def test_production_page(self):
+        page = self.make_page()
+
+        self.maxDiff = None
+        self.assertEqual(
+            '<!DOCTYPE html><html><head><title>Example Domain</title><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><style type="text/css">table, td {border: 1px solid gray; border-collapse: collapse;}</style></head><body><table><caption>Multiplication table</caption><tr><td></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr><tr><td>1</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr><tr><td>2</td><td>2</td><td>4</td><td>6</td><td>8</td><td>10</td><td>12</td><td>14</td><td>16</td><td>18</td></tr><tr><td>3</td><td>3</td><td>6</td><td>9</td><td>12</td><td>15</td><td>18</td><td>21</td><td>24</td><td>27</td></tr><tr><td>4</td><td>4</td><td>8</td><td>12</td><td>16</td><td>20</td><td>24</td><td>28</td><td>32</td><td>36</td></tr><tr><td>5</td><td>5</td><td>10</td><td>15</td><td>20</td><td>25</td><td>30</td><td>35</td><td>40</td><td>45</td></tr><tr><td>6</td><td>6</td><td>12</td><td>18</td><td>24</td><td>30</td><td>36</td><td>42</td><td>48</td><td>54</td></tr><tr><td>7</td><td>7</td><td>14</td><td>21</td><td>28</td><td>35</td><td>42</td><td>49</td><td>56</td><td>63</td></tr><tr><td>8</td><td>8</td><td>16</td><td>24</td><td>32</td><td>40</td><td>48</td><td>56</td><td>64</td><td>72</td></tr><tr><td>9</td><td>9</td><td>18</td><td>27</td><td>36</td><td>45</td><td>54</td><td>63</td><td>72</td><td>81</td></tr></table></body></html>',
+            page.text(with_html.TextParams(level=0, offset='', newline=''))
         )
 
 
