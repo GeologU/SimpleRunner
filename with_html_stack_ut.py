9ed49424724b99ec0905a0d07eb5a8f910770f23
GeologU <geologu@yandex.ru>
2021-03-04 00:14:44 +0300

    more unit tests

--- with_html_stack_ut.py
+++ with_html_stack_ut.py
@@ -5,6 +5,235 @@ import unittest
 import with_html_stack
 
 
+class TestFunctions(unittest.TestCase):
+
+    def test_to_safe_name(self):
+        self.assertEqual(with_html_stack.to_safe_name(name='testname'), 'testname')
+        self.assertEqual(with_html_stack.to_safe_name(name='testname', safe_prefix='xx_'), 'testname')
+
+        self.assertEqual(with_html_stack.to_safe_name(name='test-name'), '_test_name')
+        self.assertEqual(with_html_stack.to_safe_name(name='test-name', safe_prefix='xx_'), 'xx_test_name')
+        self.assertEqual(with_html_stack.to_safe_name(name='test-name-2'), '_test_name_2')
+
+        self.assertEqual(with_html_stack.to_safe_name(name='id'), '_id')
+        self.assertEqual(with_html_stack.to_safe_name(name='id', safe_prefix='xx_'), 'xx_id')
+
+    def test_from_safe_name(self):
+        self.assertEqual(with_html_stack.from_safe_name(name='testname'), 'testname')
+        self.assertEqual(with_html_stack.from_safe_name(name='testname', safe_prefix='xx_'), 'testname')
+
+        self.assertEqual(with_html_stack.from_safe_name(name='_test_name'), 'test-name')
+        self.assertEqual(with_html_stack.from_safe_name(name='xx_test_name', safe_prefix='xx_'), 'test-name')
+        self.assertEqual(with_html_stack.from_safe_name(name='_test_name_2'), 'test-name-2')
+
+        self.assertEqual(with_html_stack.from_safe_name(name='_id'), 'id')
+        self.assertEqual(with_html_stack.from_safe_name(name='xx_id', safe_prefix='xx_'), 'id')
+
+    def test_html_as_code(self):
+        self.assertEqual(with_html_stack.html_as_code('A&B'), "'A&B'")
+        self.assertEqual(with_html_stack.html_as_code('A&amp;B'), "html.escape('A&B')")
+        self.assertEqual(with_html_stack.html_as_code(None), 'None')
+
+    def test_get_indent(self):
+        self.assertEqual(with_html_stack.get_indent(''), '')
+        self.assertEqual(with_html_stack.get_indent(' '), ' ')
+        self.assertEqual(with_html_stack.get_indent(' \t\r text'), ' \t\r ')
+        self.assertEqual(with_html_stack.get_indent('text \t\r text'), '')
+
+
+class TestTextParams(unittest.TestCase):
+
+    def test_str(self):
+        self.assertEqual(
+            str(with_html_stack.DEV_PARAMS),
+            "TextParams(level=0, offset='    ', newline='\\n')"
+        )
+        self.assertEqual(
+            str(with_html_stack.DEV_PARAMS.inner),
+            "TextParams(level=1, offset='    ', newline='\\n')"
+        )
+
+        self.assertEqual(
+            str(with_html_stack.PROD_PARAMS),
+            "TextParams(level=0, offset='', newline='')"
+        )
+        self.assertEqual(
+            str(with_html_stack.PROD_PARAMS.inner),
+            "TextParams(level=1, offset='', newline='')"
+        )
+
+    def test_indent(self):
+        p = with_html_stack.TextParams()
+        self.assertEqual(p.indent, '')
+
+        p = with_html_stack.TextParams(level=2)
+        self.assertEqual(p.indent, '        ')
+
+    def test_inner(self):
+        p = with_html_stack.TextParams()
+        p2 = p.inner
+        self.assertEqual(p2.level, 1)
+        self.assertEqual(p2.level, p.level + 1)
+        self.assertEqual(p2.offset, p.offset)
+        self.assertEqual(p2.newline, p.newline)
+
+    def test_line(self):
+        p = with_html_stack.TextParams()
+        self.assertEqual(p.line(middle='some middle text'), 'some middle text\n')
+        self.assertEqual(p.line(middle='some middle text', indent='\t'), '\tsome middle text\n')
+
+        p2 = with_html_stack.TextParams(level=1, newline='\r\n')
+        self.assertEqual(p2.line(middle='some middle text'), '    some middle text\r\n')
+        self.assertEqual(p2.line(middle='some middle text', indent='\t'), '\tsome middle text\r\n')
+
+    def test_text_dev_params(self):
+        p = with_html_stack.DEV_PARAMS.inner
+        self.assertEqual(p.text([
+                '\rfirst line',
+                '\tsecond line',
+                ' third line',
+            ]), '''\
+    \rfirst line
+    \tsecond line
+     third line
+''')
+
+        p = with_html_stack.DEV_PARAMS.inner
+        self.assertEqual(p.text([
+                '\tfirst line',
+                '\t\tsecond line',
+                '\tthird line',
+            ], '-p-', '-s-'), '''\
+    -p-first line-s-
+    \t-p-second line-s-
+    -p-third line-s-
+''')
+
+        p = with_html_stack.DEV_PARAMS.inner
+        self.assertEqual(p.text([]), '')
+
+        p = with_html_stack.DEV_PARAMS.inner
+        self.assertEqual(p.text(['one line']), '    one line\n')
+
+    def test_text_prod_params(self):
+        p = with_html_stack.PROD_PARAMS.inner
+        self.assertEqual(p.text([
+                '\tfirst line',
+                '\t\tsecond line',
+                '\tthird line',
+            ], '-p-', '-s-'),
+            '-p-first line-s-\t-p-second line-s--p-third line-s-'
+        )
+
+    def test_none_fix(self):
+        p = with_html_stack.PROD_PARAMS.inner
+        self.assertEqual(p.text([
+                '\tfirst line',
+                '\t\tsecond line',
+                '\tthird line',
+            ], None, None),
+            'first line\tsecond linethird line'
+        )
+
+
+class TestHTMLRaw(unittest.TestCase):
+
+    def test_text(self):
+        raw = with_html_stack.HTMLRaw('some raw text', prefix='-p-', suffix='-s-')
+        self.assertEqual(
+            raw.text(with_html_stack.DEV_PARAMS.inner),
+            '    -p-some raw text-s-\n'
+        )
+        self.assertEqual(
+            raw.text(with_html_stack.PROD_PARAMS),
+            '-p-some raw text-s-'
+        )
+
+        raw = with_html_stack.HTMLRaw('some multiline\nraw text\n', prefix='-p-', suffix='-s-')
+        self.assertEqual(
+            raw.text(with_html_stack.DEV_PARAMS.inner),
+            '    -p-some multiline-s-\n    -p-raw text-s-\n'
+        )
+        self.assertEqual(
+            raw.text(with_html_stack.PROD_PARAMS),
+            '-p-some multiline-s--p-raw text-s-'
+        )
+
+
+    def test_code(self):
+        raw = with_html_stack.HTMLRaw('some raw text')
+        self.assertEqual(
+            raw.code(with_html_stack.DEV_PARAMS.inner),
+            "    doc.raw('some raw text')\n"
+        )
+
+        raw = with_html_stack.HTMLRaw('some raw text', prefix='-p-', suffix='-s-')
+        self.assertEqual(
+            raw.code(with_html_stack.DEV_PARAMS.inner),
+            "    doc.raw('some raw text', prefix='-p-', suffix='-s-')\n"
+        )
+        self.assertEqual(
+            raw.code(with_html_stack.PROD_PARAMS),
+            "doc.raw('some raw text', prefix='-p-', suffix='-s-')"
+        )
+
+        raw = with_html_stack.HTMLRaw('some multiline\nraw text\n', prefix='-p-', suffix='-s-')
+        self.assertEqual(
+            raw.code(with_html_stack.DEV_PARAMS.inner),
+            "    doc.raw('some multiline\\nraw text\\n', prefix='-p-', suffix='-s-')\n"
+        )
+        self.assertEqual(
+            raw.code(with_html_stack.PROD_PARAMS),
+            "doc.raw('some multiline\\nraw text\\n', prefix='-p-', suffix='-s-')"
+        )
+
+
+class TestHTMLComment(unittest.TestCase):
+
+    def test_text(self):
+        raw = with_html_stack.HTMLComment('some raw text')
+        self.assertEqual(
+            raw.text(with_html_stack.DEV_PARAMS.inner),
+            '    <!-- some raw text -->\n'
+        )
+        self.assertEqual(
+            raw.text(with_html_stack.PROD_PARAMS),
+            '<!-- some raw text -->'
+        )
+
+        raw = with_html_stack.HTMLComment('some multiline\nraw text\n')
+        self.assertEqual(
+            raw.text(with_html_stack.DEV_PARAMS.inner),
+            '    <!-- some multiline -->\n    <!-- raw text -->\n'
+        )
+        self.assertEqual(
+            raw.text(with_html_stack.PROD_PARAMS),
+            '<!-- some multiline --><!-- raw text -->'
+        )
+
+
+    def test_code(self):
+        raw = with_html_stack.HTMLComment('some raw text')
+        self.assertEqual(
+            raw.code(with_html_stack.DEV_PARAMS.inner),
+            "    doc.comment('some raw text')\n"
+        )
+        self.assertEqual(
+            raw.code(with_html_stack.PROD_PARAMS),
+            "doc.comment('some raw text')"
+        )
+
+        raw = with_html_stack.HTMLComment('some multiline\nraw text\n')
+        self.assertEqual(
+            raw.code(with_html_stack.DEV_PARAMS.inner),
+            "    doc.comment('some multiline\\nraw text\\n')\n"
+        )
+        self.assertEqual(
+            raw.code(with_html_stack.PROD_PARAMS),
+            "doc.comment('some multiline\\nraw text\\n')"
+        )
+
+
 class TestHTMLDocument(unittest.TestCase):
 
     def setUp(self):
