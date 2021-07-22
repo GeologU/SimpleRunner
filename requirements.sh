ce300f3b82b6ddf12019fd8a6145f8833dad4066
GeologU <geologu@yandex.ru>
2021-07-22 23:02:32 +0300

    add requirements.txt

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail -x
+
+# Install pip tools:
+#python3 -m pip install pip-tools
+
+# Update requirements.txt file:
+#python3 -m piptools compile requirements.in --output-file requirements.txt
+
+# Install from requirements.txt:
+#python3 -m pip install -r requirements.txt
+
+# Install only packages listed:
+#python3 -m piptools sync requirements.txt
